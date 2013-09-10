ea4d8136cf9ecca4e9458b81e26a5972d1eeea03
Tucker Joseph <tucker@chartboost.com>
2013-09-10 11:51:42 -0700

    get_file was not available

--- lib/scout-helpers.rb
+++ lib/scout-helpers.rb
@@ -3,7 +3,7 @@ require "scout-helpers/version"
 module ScoutHelpers
   # returns file for a category
   #
-  def get_file(path, category)
+  def self.get_file(path, category)
     path = path.slice(0, path.size - 1) if path.end_with?('/')
     File.new(
       "#{path}/#{category}/" +
